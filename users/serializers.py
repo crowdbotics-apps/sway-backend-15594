bd75e87504beaf52e89aa96e50234778bcdeedd9
Andrei Elizaga <elizaga.noel@crowdbotics.com>
2020-04-18 01:13:17 +0800

    Move CreateVendorUserSerializer.

--- users/serializers.py
+++ users/serializers.py
@@ -49,6 +49,48 @@ class CreateUserSerializer(UserCreatePasswordRetypeSerializer):
         return user
 
 
+class CreateVendorUserSerializer(CreateUserSerializer):
+    """
+    Custom CreateUserSerializer with fields specific for a Vendor User.
+    """
+    country_code = serializers.CharField(max_length=3,
+                    read_only=True,
+                    help_text='Phone number country code with optional `+` prefix')
+    phone_number = serializers.CharField(required=True,
+                    help_text='Phone number')
+
+    class Meta(CreateUserSerializer.Meta):
+        fields = tuple(DEFAULT_USER_FIELDS) + (
+            'country_code',
+            'phone_number',
+            'address',
+            'business_name',
+        )
+        read_only_fields = ('country_code',)
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+
+    def validate_phone_number(self, value):
+        """
+        Validate `country_code` with `phone_number` using the
+        `phonenumber_field` validation methods.
+        """
+        # Format country_code with + sign.
+        country_code = self.initial_data['country_code']
+        if not country_code.startswith('+'):
+            country_code = f'+{country_code}'
+
+        phone_number = self.initial_data['phone_number']
+        phone_number = f'{country_code}{phone_number}'
+        phone_number = to_python(phone_number)
+        if phone_number and not phone_number.is_valid():
+            raise exceptions.ValidationError(
+                     PhoneNumberField.default_error_messages['invalid'])
+        return phone_number
+
+
 class PhoneSerializer(serializers.Serializer):
     """
     Serializer for `phone_number` verification.
@@ -94,45 +136,3 @@ class PhoneVerificationCodeSerializer(serializers.Serializer):
             return data
         else:
             raise exceptions.ValidationError(authy_phone.errors())
-
-
-class CreateVendorUserSerializer(CreateUserSerializer):
-    """
-    Custom CreateUserSerializer with fields specific for a Vendor User.
-    """
-    country_code = serializers.CharField(max_length=3,
-                    read_only=True,
-                    help_text='Phone number country code with optional `+` prefix')
-    phone_number = serializers.CharField(required=True,
-                    help_text='Phone number')
-
-    class Meta(CreateUserSerializer.Meta):
-        fields = tuple(DEFAULT_USER_FIELDS) + (
-            'country_code',
-            'phone_number',
-            'address',
-            'business_name',
-        )
-        read_only_fields = ('country_code',)
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
-
-
-    def validate_phone_number(self, value):
-        """
-        Validate `country_code` with `phone_number` using the
-        `phonenumber_field` validation methods.
-        """
-        # Format country_code with + sign.
-        country_code = self.initial_data['country_code']
-        if not country_code.startswith('+'):
-            country_code = f'+{country_code}'
-
-        phone_number = self.initial_data['phone_number']
-        phone_number = f'{country_code}{phone_number}'
-        phone_number = to_python(phone_number)
-        if phone_number and not phone_number.is_valid():
-            raise exceptions.ValidationError(
-                     PhoneNumberField.default_error_messages['invalid'])
-        return phone_number
